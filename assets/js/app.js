81edf26d22348c0d589c28c68784dd3f48e98b1f
Fiza <131092372+Fizahere@users.noreply.github.com>
2025-05-20 01:38:40 +0500

    [making ui]

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+//mob menu
+function toggleMenu() {
+    document.getElementById("mobileMenu").classList.toggle("hidden");
+}
+
+//form
+// Initialization for ES Users
+import {
+    Input,
+    Ripple,
+    initTWE,
+} from "tw-elements";
+
+initTWE({ Input, Ripple });
